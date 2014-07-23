05c496d28935cb5eb676d90a52e7a69d7065ebcd
Scott West <scott.gregory.west@gmail.com>
2014-07-23 12:41:18 +0200

    Split larger functions out into chunk.cpp

---
+++ src/chunk.cpp
@@ -0,0 +1,83 @@
+#include "chunk.hpp"
+
+chunk*
+chunk::create (std::size_t object_size)
+{
+  std::size_t rounded_size = chunk::rounded_size (object_size);
+
+  void *ptr;
+  posix_memalign (&ptr, CHUNK_SIZE, rounded_size);
+
+  std::size_t num_objects = rounded_size / object_size;
+  assert (object_size >= CHUNK_SIZE/2
+	  ? num_objects == 1 
+	  : num_objects > 1);
+
+  chunk *c = new (ptr) chunk (object_size, num_objects);
+
+  return c;
+}    
+
+std::size_t
+chunk::rounded_size(std::size_t object_size)
+{
+  std::size_t num_chunks = (object_size / CHUNK_SIZE) + 1;
+  std::size_t rounded_size = num_chunks * CHUNK_SIZE;
+
+  std::size_t remaining_space = rounded_size - sizeof(chunk);
+  if (remaining_space < object_size)
+    {
+      rounded_size += CHUNK_SIZE;
+    }
+
+  return rounded_size;
+}
+
+
+void
+chunk::destroy (chunk* chunk)
+{
+  chunk->~chunk();
+  free (chunk);
+}
+
+
+chunk*
+chunk::split (std::size_t new_size)
+{
+  assert (can_split_into (new_size));
+
+  auto new_other_size = rounded_size(new_size);
+  auto new_this_size  = size() - new_other_size;
+  object_size         = new_this_size - sizeof(chunk);
+
+  assert ((char*)this + new_this_size == end());
+
+  chunk *c = new (end()) chunk (new_size, new_other_size / new_size);
+
+  return c;
+}
+
+void
+chunk::mark (void* ptr)
+{
+  int i = ((char*) ptr - (char*) data()) / object_size;
+  mark_bitmap.set (i, true);
+}
+
+
+bool
+chunk::is_marked (void* ptr)
+{
+  int i = ((char*) ptr - (char*) data()) / object_size;
+  return mark_bitmap [i];
+}
+
+
+bool
+chunk::valid (void *ptr)
+{
+  std::size_t offset = (char*)ptr - (char*)data();
+  bool in_chunk_bounds = offset < CHUNK_SIZE - sizeof(chunk);
+  return in_chunk_bounds && offset % object_size == 0;
+}
